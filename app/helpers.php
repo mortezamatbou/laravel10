dc721d726a74c86a9c196ddda92093599f79b92d
Morteza Matbou <mortezamatbou91@gmail.com>
2023-11-09 14:53:13 +0330

    add my helper pre_print() function

---
+++ app/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+function pre_print($data, $pre = 'pre'): void
+{
+    if ($pre == 'pre') {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    } elseif ($pre == 'json') {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    } else {
+        print_r($data);
+    }
+    exit;
+}
