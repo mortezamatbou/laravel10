43784e71680ecfe5c0ac341e8bb3f1ae35484dd1
Morteza Matbou <mortezamatbou91@gmail.com>
2023-11-25 23:37:34 +0330

    add controller actions for user login and logout

--- app/Http/Controllers/CoinsController.php
+++ app/Http/Controllers/CoinsController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CoinsStoreRequest;
 use App\Http\Requests\CoinsUpdateRequest;
 use App\Models\Coins;
+use App\Models\User;
 use App\Repositories\CoinsRepository;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Permission;
 
 class CoinsController extends Controller
 {
@@ -20,8 +23,56 @@ class CoinsController extends Controller
         $this->coins = new CoinsRepository(new Coins());
     }
 
+    public function login_form(): View
+    {
+        $title = 'Login';
+        return view('coins.login', compact('title'));
+    }
+
+    public function login_check(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // login successfully
+            $request->session()->regenerate();
+            return redirect()->intended('/coins');
+        }
+
+        // else, username or password is invalid
+        return back()->withErrors([
+            'email' => 'Username or Password is invalid'
+        ])->onlyInput('email');
+    }
+
+    public function login_success(Request $request): void
+    {
+        $id = Auth::id();
+        $user = Auth::user();
+    }
+
     public function index(): View
     {
+
+//        $users = User::with('roles')->role('viewer')->get();
+//        $users = User::doesntHave('roles')->get();
+//
+//        foreach ($users as $user) {
+//            echo "$user->id $user->name $user->email<br>";
+//        }
+//        exit;
+//
+//        $user = User::where('id', 7)->first();
+//        pre_print($user->getAllPermissions());
+//        /**
+//         * @var $permission Permission
+//         */
+//        $permission = $user->getPermissionsViaRoles()[1];
+//        pre_print($permission->guard_name);
+
         $coins = $this->coins->all();
         $title = 'Coins';
 
@@ -44,7 +95,7 @@ class CoinsController extends Controller
 
         $data = $request->validated();
         $this->coins->update($data, $id);
-        return to_route('coins.detail',  ['id' => $id]);
+        return to_route('coins.detail', ['id' => $id]);
     }
 
     public function coin_add_form(): View
@@ -57,6 +108,14 @@ class CoinsController extends Controller
         $data = $request->validated();
         $id = $this->coins->create($data);
         return to_route('coins.detail', ['id' => $id]);
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('coins')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return to_route('coins.login.form');
     }
 
 }
